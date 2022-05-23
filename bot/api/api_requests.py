1b37b88034e767a3a7d746030c1533fa5022a07c
CreativeeBlackWolf <creativeeblackwolf@gmail.com>
2022-05-23 15:41:09 +0300

    api_requests docs

--- bot/api/api_requests.py
+++ bot/api/api_requests.py
@@ -8,6 +8,10 @@ import requests
 
 
 class DefaultAPIRequest:
+    """
+    Default config for API.
+    """
+
     def __init__(self) -> None:
         config = get_api_settings()
         self.default_url = f"http://{config.host}:{config.port}/api/v1/"
@@ -16,38 +20,59 @@ class DefaultAPIRequest:
 
 
 class UserAPIRequest(DefaultAPIRequest):
+    """
+    Requests to Users API.
+
+    Reference: https://github.com/CreativeeBlackWolf/RESTproject#users
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self.users_url = self.default_url + "users/"
 
-    def get_user(self, user_id: int) -> Tuple[dict, int]:
-        request = self.session.get(self.users_url + f"?vk_id={user_id}")
+    def get_user(self, vk_id: int) -> Tuple[dict, int]:
+        """
+        Get user by it's vk_id.
+        """
+        request = self.session.get(self.users_url + f"?vk_id={vk_id}")
         return request.json()[0], request.status_code
 
     def get_users(self) -> Tuple[List[dict], int]:
+        """
+        Get full users list.
+        """
         request = self.session.get(self.users_url)
         return request.json(), request.status_code
 
-    def create_user(self, user_id: int, name: str) -> Tuple[dict, int]:
-        data = {"vk_id": user_id, "user": name}
+    def create_user(self, vk_id: int, name: str) -> Tuple[dict, int]:
+        """
+        Create a user with given vk_id.
+        """
+        data = {"vk_id": vk_id, "user": name}
         request = self.session.post(self.users_url, data = data)
         return request.json(), request.status_code
 
 
 class WalletAPIRequest(UserAPIRequest):
+    """
+    Requests to Wallets API
+
+    Reference: https://github.com/CreativeeBlackWolf/RESTproject#wallets
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self.wallets_url = self.default_url + "wallets/"
 
     def get_user_wallets(self, user_id: int) -> Tuple[Union[List[Wallet], dict], int]:
-        real_id  = self.get_user(user_id)[0]["id"]
+        real_id = self.get_user(user_id)[0]["id"]
         request = self.session.get(self.wallets_url + f"?user={real_id}")
         if request.status_code != 200:
             return request.json(), request.status_code
         return serialize_wallet(request.json()), request.status_code
 
     def create_new_wallet(self, user_id: int, wallet_name: str) -> Tuple[Union[Wallet, dict], int]:
-        real_id  = self.get_user(user_id)[0]["id"]
+        real_id = self.get_user(user_id)[0]["id"]
         data = {
             "user": real_id,
             "name": wallet_name
@@ -78,6 +103,12 @@ class WalletAPIRequest(UserAPIRequest):
         return request.status_code
 
 class TransactionsAPIRequest(UserAPIRequest):
+    """
+    Requests to Transacions API.
+
+    Reference: https://github.com/CreativeeBlackWolf/RESTproject#transactions
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self.transactions_api = self.default_url + "transactions/"
@@ -91,8 +122,24 @@ class TransactionsAPIRequest(UserAPIRequest):
         comment: Optional[str] = None,
         *args, **kwargs
     ) -> Tuple[Union[Transaction, dict], int]:
+        """
+        Do a transaction request.
+
+        Reference:
+        https://github.com/CreativeeBlackWolf/RESTproject#apiv1transactions
+
+        Unreferenced parameters
+        -----------------------
+        `args` and `kwargs`
+            Since some extra data can be stored in temporary storage, 
+            these parameters are used to prevent an exception from occurring,
+            making it possible to unpack arguments.
+            They are not used anywhere.
+        """
         if (to_wallet and whence) or (to_wallet is None and whence is None):
-            raise TypeError("One of fields 'from_wallet' or 'whence' must have a value, neither both nor none of them.")
+            raise TypeError(
+                "One of fields 'from_wallet' or 'whence' must have a value, neither both nor none of them."
+            )
         data = {
             "from_wallet": from_wallet,
             "to_wallet": to_wallet,
@@ -105,8 +152,22 @@ class TransactionsAPIRequest(UserAPIRequest):
             return request.json(), request.status_code
         return serialize_transaction(request.json()), request.status_code
 
-    def get_user_transactions(self, user_id: int, limit: int=5, incoming=False) -> Tuple[Transaction, int]:
-        real_id  = self.get_user(user_id)[0]["id"]
+    def get_user_transactions(self, vk_id: int, limit: int=5, incoming=False) -> Tuple[Transaction, int]:
+        """
+        Get transactions only for specified user.
+
+        Parameters
+        ----------
+        `vk_id`
+            ID in VK (wow)
+        
+        `limit`
+            Limit the number of returned transactions. Default is 5.
+        
+        `incoming`
+            Show incoming transactions. Default is False.
+        """
+        real_id = self.get_user(vk_id)[0]["id"]
         if not incoming:
             request = self.session.get(self.transactions_api + f"?from_user={real_id}")
         else:
--- bot/utils/check.py
+++ bot/utils/check.py
@@ -3,6 +3,20 @@ from typing import List
 
 
 def find_urls(text: str, template: str = None) -> List[str]:
+    """
+    Find URLs in given string.
+
+    Returns list of URLs.
+
+    Paremeters
+    ----------
+    `text`
+        Text in which to find links.
+    
+    `template`
+        A link template. If given, 
+        function will return links that satisfy the specified template.
+    """
     regex = \
 r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, text)
